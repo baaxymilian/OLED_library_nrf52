837cedcfe01f84e6728e06d6100f1fa3e2df3ac9
Imbryczek <bartosz.bryk.lo@gmail.com>
2019-04-11 17:10:46 +0200

    First upload

---
+++ fonts.h
@@ -0,0 +1,37 @@
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _USE_FONTS_H
+#define _USE_FONTS_H
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "stdint-gcc.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+extern const uint8_t c_chFont1206[95][12];
+extern const uint8_t c_chFont1608[95][16];
+extern const uint8_t c_chFont1612[11][32];
+extern const uint8_t c_chFont3216[11][64];
+//extern const uint8_t c_chFont3216[18][32];
+extern const uint8_t c_chBmp4016[96];
+extern const uint8_t c_chSingal816[16];
+extern const uint8_t c_chMsg816[16];
+extern const uint8_t c_chBluetooth88[8];
+extern const uint8_t c_chBat816[16];
+extern const uint8_t c_chGPRS88[8];
+extern const uint8_t c_chAlarm88[8];
+extern const uint8_t c_chUsb816[16];
+extern const uint8_t c_chSD816[16];
+
+extern const uint8_t ProcentBitmaps[11][384];
+
+
+
+#endif
+
+/*-------------------------------END OF FILE-------------------------------*/
+
